c87f360f8c38b75b76eada8478a4a4f4349a1440
Tii04 <tiisetso.qsebata@gmail.com>
2023-08-22 13:50:32 +0200

    task_3

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+    all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
+  },
+	output: {
+		path: path.resolve(__dirname, './public'),
+		filename: '[name].bundle.js',
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard',
+    }),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+				use: ['file-loader'],
+			},
+		],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: {
+    directory: path.join(__dirname, 'public'),
+    },
+		port: 8564,
+	},
+	mode: 'development',
+};
